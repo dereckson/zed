f1593e5a89eab933f5bd649f33f8982bcb3e2112
Dereckson <dereckson@espace-win.org>
2010-01-30 02:45:42 +0100

    Updating config: geo, api tables

--- includes/config.php
+++ includes/config.php
@@ -19,19 +19,23 @@ $Config['sql']['database'] = 'zed';
 
 //SQL tables
 $prefix = '';
-define('TABLE_BODIES', $prefix . 'geo_bodies');
+define('TABLE_API_KEYS', $prefix . 'api_keys');
 define('TABLE_COMMENTS', $prefix . 'comments');
 define('TABLE_LOG_SMARTLINE', $prefix . 'log_smartline');
 define('TABLE_MESSAGES', $prefix . 'messages');
 define('TABLE_MOTD', $prefix . 'motd');
 define('TABLE_PERSOS', $prefix . 'persos');
-define('TABLE_PLACES', $prefix . 'geo_places');
+define('TABLE_PERSOS_FLAGS', $prefix . 'persos_flags');
 define('TABLE_PROFILES', $prefix . 'profiles');
 define('TABLE_PROFILES_COMMENTS', $prefix . 'profiles_comments');
 define('TABLE_PROFILES_PHOTOS', $prefix . 'profiles_photos');
 define('TABLE_SESSIONS', $prefix . 'sessions');
 define('TABLE_USERS', $prefix . 'users');
 define('TABLE_USERS_OPENID', $prefix . 'users_openid');
+
+define('TABLE_BODIES', $prefix . 'geo_bodies');
+define('TABLE_LOCATIONS', $prefix . 'geo_locations');       //View
+define('TABLE_PLACES', $prefix . 'geo_places');
 
 //Script URL
 $Config['BaseURL'] = '/index.php';
