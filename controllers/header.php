d4e2a3d6154c9ee6e3799c0bd7ac55ec1721de03
Dereckson <dereckson@espace-win.org>
2010-02-08 01:30:10 +0100

    [Wall] Prints name instead username

--- controllers/header.php
+++ controllers/header.php
@@ -13,11 +13,11 @@
 //
 
 //TODO: this is a potentially very intensive SQL query
-$sql = 'SELECT p.perso_nickname as username, m.motd_text FROM ' . TABLE_PERSOS . ' p, ' . TABLE_MOTD . ' m WHERE p.perso_id = m.perso_id ORDER BY rand() LIMIT 1';
+$sql = 'SELECT p.perso_nickname as username, p.perso_name as name, m.motd_text FROM ' . TABLE_PERSOS . ' p, ' . TABLE_MOTD . ' m WHERE p.perso_id = m.perso_id ORDER BY rand() LIMIT 1';
 if (!$result = $db->sql_query($sql)) message_die(SQL_ERROR, "Can't query MOTD", '', __LINE__, __FILE__, $sql);
 $row = $db->sql_fetchrow($result);
 $smarty->assign('WALL_TEXT', $row['motd_text']);
-$smarty->assign('WALL_USER', $row['username']);
+$smarty->assign('WALL_USER', $row['name']);
 $smarty->assign('WALL_USER_URL', get_url('who', $row['username']));
 
 //
