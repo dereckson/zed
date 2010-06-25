17177b18cfa86b78e7292de97620d4084ccb422c
Dereckson <dereckson@espace-win.org>
2010-06-25 21:26:45 +0000

    Were eregi_replace, so should be preg_replace('/.../i', ...);

--- includes/Smarty/plugins/modifier.text2html.php
+++ includes/Smarty/plugins/modifier.text2html.php
@@ -22,12 +22,12 @@ function smarty_modifier_text2html($string)
     $string = htmlspecialchars($string, ENT_QUOTES);
     
     //E-mail -> mailto links
-    $string = preg_replace("/([_a-z0-9-]+(\._a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+)/", "<a href=\"mailto:\\1\">\\1</A>", $string);
+    $string = preg_replace("/([_a-z0-9-]+(\._a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+)/i", "<a href=\"mailto:\\1\">\\1</A>", $string);
 
     //Linkify URLs
     //TODO handle www.
     //TODO Relative links for current domain
-    $string = preg_replace("@([[:alnum:]]+)://([^[:space:]]*)([[:alnum:]#?/&=])@", "<a href=\"\\1://\\2\\3\">\\1://\\2\\3</a>", $string);
+    $string = preg_replace("@([[:alnum:]]+)://([^[:space:]]*)([[:alnum:]#?/&=])@i", "<a href=\"\\1://\\2\\3\">\\1://\\2\\3</a>", $string);
     
     //\n -> <br />
     $string = nl2br($string);
