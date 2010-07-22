3fb320743f797378e8d393fad74cd4329e6a0d55
Dereckson <dereckson@espace-win.org>
2010-07-22 03:58:42 +0000

    [SmartLine] Zed specific commands documentation

--- includes/SmartLine/ZedCommands.php
+++ includes/SmartLine/ZedCommands.php
@@ -1,29 +1,52 @@
 <?php
 
-/*
- * Zed
- * (c) 2010, Dereckson, some rights reserved
- * Released under BSD license
+/**
+ * Zed SmartLine commands.
  *
- * SmartLine
+ * Zed. The immensity of stars. The HyperShip. The people.
+ * 
+ * (c) 2010, Dereckson, some rights reserved.
+ * Released under BSD license.
  *
- * TODO: SettingsSmartLineCommand - understand why dojo floating pane isn't
- *       rendered if we est $controller instead to redirect
+ * This is the SmartLine subcontroller.
+ *
+ * The SmartLine is a widget allowing to add some basic CLI capability.
+ *
+ * It executes any command given in GET or POST request (parameter C).
+ *
+ * This files also provides SmartLine history helper: a method log_C to log
+ * a SmartLine command and some procedural code assigning a SmartLineHistory.
+ *
+ * This code is inspired from Viper, a corporate PHP intranet I wrote in 2004.
+ * There, the SmartLine allowed to change color theme or to find quickly user,
+ * account, order or server information in a CRM context.
+ * 
+ * @package     Zed
+ * @subpackage  SmartLine
+ * @author      Sébastien Santoro aka Dereckson <dereckson@espace-win.org>
+ * @copyright   2010 Sébastien Santoro aka Dereckson
+ * @license     http://www.opensource.org/licenses/bsd-license.php BSD
+ * @version     0.1
+ * @link        http://scherzo.dereckson.be/doc/zed
+ * @link        http://zed.dereckson.be/
+ * @filesource
+ *
+ * @todo SettingsSmartLineCommand - understand why dojo floating pane isn't rendered if we est $controller instead to redirect
  */
 
 ///
 /// Register commands
 ///
 
-$smartLine->register_object('goto', 'GotoSmartLineCommand');
-$smartLine->register_object('guid', 'GUIDSmartLineCommand');
-$smartLine->register_object('invite', 'InviteSmartLineCommand');
-$smartLine->register_object('invites', 'InviteSmartLineCommand');
-$smartLine->register_object('list', 'ListSmartLineCommand');
+$smartLine->register_object('goto',     'GotoSmartLineCommand');
+$smartLine->register_object('guid',     'GUIDSmartLineCommand');
+$smartLine->register_object('invite',   'InviteSmartLineCommand');
+$smartLine->register_object('invites',  'InviteSmartLineCommand');
+$smartLine->register_object('list',     'ListSmartLineCommand');
 $smartLine->register_object('requests', 'RequestsSmartLineCommand');
 $smartLine->register_object('settings', 'SettingsSmartLineCommand');
 $smartLine->register_object('unixtime', 'UnixTimeSmartLineCommand');
-$smartLine->register_object('version', 'VersionSmartLineCommand');
+$smartLine->register_object('version',  'VersionSmartLineCommand');
 $smartLine->register_object('whereami', 'WhereAmISmartLineCommand');
 
 
@@ -36,15 +59,24 @@ $lang['Help']['guid'] = "Generate a GUID";
 $lang['Help']['invite'] = "Generate an invite. To see the generated invites, invite list.";
 $lang['Help']['list'] = "Lists specified objects (bodies, locations or places)";
 $lang['Help']['requests'] = "Checks if there are waiting requests";
+$lang['Help']['settings'] = 'Go to settings page';
 $lang['Help']['unixtime'] = "Prints current unixtime (seconds elapsed since 1970-01-01 00:00, UTC) or the specified unixtime date.";
 $lang['Help']['version'] = "Gets Zed's software version info (Mercurial repository version, node id and if you're on the dev or prod site)";
 $lang['Help']['whereami'] = "Where am I?";
 
-///
-/// goto
-///
 
+/**
+ * The goto command
+ *
+ * Moves to the current perso to the specified location.
+ */
 class GotoSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
         global $CurrentPerso;
         
@@ -99,11 +131,20 @@ class GotoSmartLineCommand extends SmartLineCommand {
     }    
 }
 
-///
-/// GUID
-///
-
+/**
+ * The GUID command
+ *
+ * Prints a new GUID.
+ *
+ * guid 8 will print 8 guid
+ */
 class GUIDSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
 		if ($argc > 1 && is_numeric($argv[1])) {
 			for ($i = 0 ; $i < $argv[1] ; $i++) {
@@ -116,11 +157,27 @@ class GUIDSmartLineCommand extends SmartLineCommand {
     }
 }
 
-///
-/// invite
-///
-
+/**
+ * The invite command
+ *
+ * Manages invites.
+ *
+ * invite [add]
+ *     creates a new invite code
+ * 
+ * invite del <invite code>
+ *     deletes the specified invite
+ * 
+ * invite list
+ *     prints current invite codes
+ */
 class InviteSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
 		require_once('includes/objects/invite.php');
 		global $CurrentUser, $CurrentPerso;
@@ -168,11 +225,20 @@ class InviteSmartLineCommand extends SmartLineCommand {
     }
 }
 
-///
-/// list
-///
-
+/**
+ * The list command
+ *
+ * Prints a list of bodies, locations or places.
+ *
+ * This can easily be extended to output any list from any table.
+ */
 class ListSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
         if ($argc == 1) {
             $this->SmartLine->puts("Available lists: bodies, locations, places");
@@ -221,6 +287,16 @@ class ListSmartLineCommand extends SmartLineCommand {
     
     }
     
+    /**
+     * Gets a custom list from the specified table and fields.
+     *
+     * The list will ascendingly ordered by the specified key.
+     *
+     * @param $table the table to query from the database
+     * @param $key the first field to fetch, as key
+     * @param $value the second field to fetch, as value
+     * @param $where the WHERE clause, without the WHERE keyword (optionnal)
+     */
     public function get_list ($table, $key, $value, $where = null) {
         global $db;
         $sql = "SELECT $key as `key`, $value as value FROM $table ";
@@ -232,17 +308,28 @@ class ListSmartLineCommand extends SmartLineCommand {
         while ($row = $db->sql_fetchrow($result)) {
             $rows .= "<tr><td>$row[key]</td><td>$row[value]</td></tr>";
         }
-        $this->SmartLine->truncate(STDERR);
+        $this->SmartLine->truncate(STDERR); //kludge
         return "<table cellspacing=\"8\"><thead style=\"color: white\" scope=\"row\"><tr><th>Key</th><th>Value</th></thead><tbody>$rows</tbody></table>";
     }
 }
 
 
-///
-/// Requests
-///
-
+/**
+ * The requests command
+ *
+ * Redirects user the the requests page.
+ *
+ * By default only redirect if a flag indicates there's a new request.
+ *
+ * To forcefully goes to the request page, requests --force
+ */
 class RequestsSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
 		global $CurrentPerso;
 		$force = ($argc > 1) && ($argv[1] == "-f" || $argv[1] == "--force");
@@ -255,11 +342,18 @@ class RequestsSmartLineCommand extends SmartLineCommand {
     }
 }
 
-///
-/// Settings
-///
-
+/**
+ * The settings command
+ *
+ * Redirects user the the settings page.
+ */
 class SettingsSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
 		if (headers_sent()) {
 			global $controller;
@@ -270,11 +364,19 @@ class SettingsSmartLineCommand extends SmartLineCommand {
     }
 }
 
-///
-/// unixtime
-///
-
+/**
+ * The unixtime command
+ *
+ * Prints current unixtime (seconds elapsed since 1970-01-01 00:00, UTC)
+ * or if an unixtime is specified as argument, the matching date.
+ */
 class UnixTimeSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
         date_default_timezone_set('UTC');
         if ($argc == 1) {
@@ -294,11 +396,26 @@ class UnixTimeSmartLineCommand extends SmartLineCommand {
     }
 }
 
-///
-/// version
-///
-
+/**
+ * The version command
+ *
+ * Prints current hg revision, if we're in prod or dev environement and
+ * the current revision's hash.
+ *
+ * The version and env information is extracted from
+ *      .hg/tags.cache (indicating we're in a Mercurial repo and so in a dev environment), or from
+ *      .hg_archival.txt file (indicating we've deployed code in a production environement)
+ * 
+ * e.g. r130 (development environment)
+ *      Hash: 057bf394741706fd2136541e3bb07c9e60b4963d 
+ */
 class VersionSmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
 		//Gets .hg revision
 		if (file_exists('.hg/tags.cache')) {
@@ -323,11 +440,18 @@ class VersionSmartLineCommand extends SmartLineCommand {
 	}
 }
 
-///
-/// whereami
-///
-
+/**
+ * The whereami (Where am I?) command
+ *
+ * Prints current position, e.g. B00001001 - Tour, Hypership
+ */
 class WhereAmISmartLineCommand extends SmartLineCommand {
+    /**
+     * Runs the command
+     *  
+     * @param array $argv an array of string, each item a command argument
+     * @param int $argc the number of arguments
+     */
     public function run ($argv, $argc) {
 	    global $CurrentPerso;
 		
