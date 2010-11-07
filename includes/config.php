32e9016e929cdd32350dff35a355f39eb0368b8e
dereckson <dereckson@espace-win.org>
2010-11-07 17:22:56 +0000

    [Lang] * en/profile.conf: escaping strings with '
    [Config]
    * Adding explanation on how to configure URL with nginx
    * Disables cache by default
    * Cleaning extra white spaces

--- includes/config.php
+++ includes/config.php
@@ -4,10 +4,10 @@
  * Autogenerable configuration file
  *
  * Zed. The immensity of stars. The HyperShip. The people.
- * 
+ *
  * (c) 2010, Dereckson, some rights reserved.
  * Released under BSD license.
- * 
+ *
  * @package     Zed
  * @subpackage  Keruald
  * @author      Sébastien Santoro aka Dereckson <dereckson@espace-win.org>
@@ -24,7 +24,7 @@
 /// I. SQL configuration                                                     ///
 ///                                                                          ///
 ////////////////////////////////////////////////////////////////////////////////
- 
+
 //SQL configuration
 $Config['sql']['product'] = 'MySQL';    //Only MySQL is currently implemented
 $Config['sql']['host'] = 'localhost';
@@ -87,8 +87,8 @@ define('BUFFER_SIZE', 4096);
 ////////////////////////////////////////////////////////////////////////////////
 
 /*
- * Without mod_rewrite:
- * 
+ * Apache httpd, without mod_rewrite:
+ *
  *   Subdirectory:
  *     - $Config['SiteURL'] = 'http://zed.dereckson.be/hypership/index.php';
  *     - $Config['BaseURL'] = '/hypership/index.php';
@@ -97,8 +97,8 @@ define('BUFFER_SIZE', 4096);
  *     - $Config['SiteURL'] = 'http://zed.dereckson.be/index.php';
  *     - $Config['BaseURL'] = '/index.php';
  *
- * With mod_rewrite:
- * 
+ * Apache httpd, with mod_rewrite:
+ *
  *   Subdirectory:
  *     - $Config['SiteURL'] = 'http://zed.dereckson.be/hypership';
  *     - $Config['BaseURL'] = '/hypership';
@@ -121,13 +121,42 @@ define('BUFFER_SIZE', 4096);
  *       RewriteCond %{REQUEST_FILENAME} !-d
  *       RewriteRule . /index.php [L]
  *
+ * nginx:
+ *
+ *   Use same config.php settings than Apache httpd, with mod_rewrite.
+ *
+ *   In your server block:
+ *       location / {
+ *           #Serves static files if they exists, with one month cache
+ *           if (-f $request_filename) {
+ *               expires 30d;
+ *               break;
+ *           }
+ *
+ *           #Sends all non existing file or directory requests to index.php
+ *           if (!-e request_filename) {
+ *               rewrite ^(.+)$ /index.php last;
+ *               #Or if you use a subdirectory:
+ *               #rewrite ^(.+)$ /hypership/index.php last;
+ *           }
+ *       }
+ *
+ *       location ~ \.php$ {
+ *           #Your instructions to pass query to your FastCGI process, like:
+ *           fastcgi_pass   127.0.0.1:9000;
+ *           fastcgi_param  SCRIPT_FILENAME  /var/www/zed$fastcgi_script_name;
+ *           include        fastcgi_params;
+ *       }
+ *
  *
  * If you don't want to specify the server domain, you can use get_server_url:
  *      $Config['SiteURL'] = get_server_url() . '/hypership';
  *      $Config['SiteURL'] = get_server_url();
- * 
+ *
+ *
+ *
  * !!! No trailing slash !!!
- *   
+ *
  */
 
 $Config['SiteURL'] = get_server_url();
@@ -146,7 +175,6 @@ $Config['DoURL'] = $Config['SiteURL'] . "/do.php";
 //Static content = 4 directories: js, css, img and content
 //On default installation, those directories are at site root.
 //To improve site performance, you can use a CDN for that.
-//To use 
 //
 //Recommanded setting: $Config['StaticContentURL'] = $Config['SiteURL'];
 //Or if Zed is the site root: $Config['StaticContentURL'] = '';
@@ -196,8 +224,16 @@ ini_set('session.gc_maxlifetime', 345600);  //4 days, for week-end story pause a
 ///                                                                          ///
 ////////////////////////////////////////////////////////////////////////////////
 
-$Config['cache']['engine'] = 'memcached';
-$Config['cache']['server'] = 'localhost';
-$Config['cache']['port']   = 11211;
+/*
+ * To use memcached:
+ *    - $Config['cache']['engine'] = 'memcached';
+ *    - $Config['cache']['server'] = 'localhost';
+ *    - $Config['cache']['port']   = 11211;
+ *
+ * To disable cache:
+ *    - $Config['cache']['engine'] = 'void';
+ *    (or don't write nothing at all)
+ */
+$Config['cache']['engine'] = 'void';
 
 ?>
