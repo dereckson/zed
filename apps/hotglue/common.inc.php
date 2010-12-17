e4b0408673369205fdcd9356a4ff87b7276e4669
dereckson <dereckson@espace-win.org>
2010-12-17 23:59:44 +0000

    [HotGlue] * Adding %DOWN and %UP url aliases

--- apps/hotglue/common.inc.php
+++ apps/hotglue/common.inc.php
@@ -354,8 +354,9 @@ function resolve_aliases($s, $name = '')
     $s = str_replace('%EAST', '/do.php/local_move/east?redirectTo=/', $s);
     $s = str_replace('%SOUTH', '/do.php/local_move/south?redirectTo=/', $s);
     $s = str_replace('%WEST', '/do.php/local_move/west?redirectTo=/', $s);
+    $s = str_replace('%UP', '/do.php/local_move/up?redirectTo=/', $s);
+    $s = str_replace('%DOWN', '/do.php/local_move/down?redirectTo=/', $s);
     $s = preg_replace('/%MOVE (.*)/', '/do.php/local_move/?redirectTo=$1?redirectTo=/', $s);
-    //$s = str_replace('%MOVE ', '/do.php/local_move/?redirectTo=WAZZA?redirectTo=/', $s);
 
 	if (!empty($name)) {
 		// current object
