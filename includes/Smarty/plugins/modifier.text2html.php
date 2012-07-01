22bedf0d9f4a33e47c4b921d1f3daff80b066235
Dereckson <dereckson@espace-win.org>
2012-07-01 03:15:40 +0200

    [Templates]     * Smarty library updated to 3.1.10 (readding files present in previous version)     * This commit brokes Zed, fixing commit coming

---
+++ includes/Smarty/plugins/modifier.text2html.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+
+/**
+ * Smarty lower modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     yyyymmdd<br>
+ * Purpose:  Appends dashes to yyyymmdd string to output yyyy-mm-dd
+ * @author   Wolfaeym
+ * @param string
+ * @return string
+ */
+function smarty_modifier_text2html($string)
+{
+    //HTML entities
+    $string = htmlspecialchars($string, ENT_QUOTES);
+    
+    //E-mail -> mailto links
+    $string = preg_replace("/([_a-z0-9-]+(\._a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+)/i", "<a href=\"mailto:\\1\">\\1</A>", $string);
+
+    //Linkify URLs
+    //TODO handle www.
+    //TODO Relative links for current domain
+    $string = preg_replace("@([[:alnum:]]+)://([^[:space:]]*)([[:alnum:]#?/&=])@i", "<a href=\"\\1://\\2\\3\">\\1://\\2\\3</a>", $string);
+    
+    //\n -> <br />
+    $string = nl2br($string);
+    
+    return $string;
+}
+
+?>
