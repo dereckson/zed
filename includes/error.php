ced31ab66b8f735b53fd3b670cf4e69a996e910c
Sébastien Santoro <dereckson@espace-win.org>
2015-02-24 12:17:03 +0100

    [Keruald] Cleaned old_message_die()
    Once upon a time, german sites offer nice, well spaced 404 error
    blocks, in an HTML markup using rather heavily corner images.
    
    old_message_die() now ouputs a simple HTML output, instead of
    this old school error block.

--- includes/error.php
+++ includes/error.php
@@ -145,6 +145,7 @@ function message_die ($msg_code, $msg_text = '', $msg_title = '', $err_line = ''
  * Outputs an error message and terminates the current script.
  *
  * This is the message_die method from Espace Win, used on Zed as fallback if Smarty isn't initialized yet.
+ * Former "german style" error HTML markups have been removed.
  *
  * @param int $msg_code an integer constant identifying the error (HACK_ERROR, SQL_ERROR, GENERAL_ERROR)
  * @param string $msg_text the error message text (optionnal, but recommanded)
@@ -161,98 +162,35 @@ function old_message_die($msg_code, $msg_text = '', $msg_title = '', $err_line =
     $sql_store = $sql;
 
     if ($msg_code == HACK_ERROR && $Utilisateur[user_id] < 1000) {
-        global $LoginResult;
-        foreach ($_POST as $name => $value) {
-            $champs .= "<input type=hidden name=$name value=\"$value\" />";
-        }
-        $titre = "Qui êtes-vous ?";
-        $debug_text = "Vous devez être authentifié pour accéder à cette page.";
-        $debug_text .= "
-		<FORM method='post'>
-		$champs
-		<table border='0'>
-		  <tr>
-			<td><STRONG>Login</STRONG></td>
-			<td><input name='Login' type='text' id='Login'  value='$_POST[Login]' size='10' /></td>
-			<td><STRONG>Mot de passe</STRONG></td>
-			<td>
-				<input name='MotDePasse' type='password' id='MotDePasse' size='10' />
-				<input type='submit' name='LoginBox' value='Connexion' />
-			</td>
-		  </tr>
-		  <tr>
-			<td align=center COLSPAN=4><a href='/?Topic=My&Article=Enregistrer'>Je d&eacute;sire ouvrir un compte</a></td>
-		  </tr>
-		</TABLE><span class=error>$LoginResult</span>
-		</FORM>
-		";
+        die("You must be logged in to access to this resource.");
     } elseif ($msg_code == HACK_ERROR) {
-        $titre = "Accès non autorisé";
+        $title = "You aren't allowed to access this resource.";
         $debug_text = $msg_text;
     } elseif ($msg_code == SQL_ERROR) {
-        $titre = "Erreur dans la requête SQL";
+        $title = "SQL error";
         $sql_error = $db->sql_error();
         $debug_text = $msg_text;
-        if ( $err_line != '' && $err_file != '') $debug_text .= ' dans ' . $err_file. ', ligne ' . $err_line ;
-        if ( $sql_error['message'] != '' ) $debug_text .= '<br />Erreur n° ' . $sql_error['code'] . ' : ' . $sql_error['message'];
-        if ( $sql_store != '' ) $debug_text .= "<br /><strong>$sql_store</strong>";
+        if ($err_line != '' && $err_file != '') {
+            $debug_text .= ' in ' . $err_file. ', line ' . $err_line ;
+        }
+        if ($sql_error['message'] != '') {
+            $debug_text .= '<br />Error #' . $sql_error['code'] . ': ' . $sql_error['message'];
+        }
+        if ($sql_store != '') {
+            $debug_text .= "<br /><strong>$sql_store</strong>";
+        }
     } elseif ($msg_code == GENERAL_ERROR) {
-        $titre = $msg_title;
+        $title = $msg_title;
         $debug_text = $msg_text;
         if ($err_line && $err_file) {
-            $debug_text .= "<BR />$err_file, ligne $err_line";
+            $debug_text .= "<br />$err_file, line $err_line";
         }
     }
 
-    echo "
-	<TABLE height='100%' cellSpacing=0 cellPadding=0 width='100%' border=0>
-  <TBODY>
-  <TR>
-    <TD vAlign=top align=middle>
-      <TABLE cellSpacing=0 cellPadding=0 border=0>
-        <TBODY>
-        <TR>
-          <TD vAlign=top rowSpan=5><IMG height=177 alt=''
-            src='/_pict/error/notfound.jpg' width=163 border=0></TD>
-          <TD colSpan=4><IMG height=2 alt='' src='/_pict/error/mrblue.gif'
-            width=500 border=0></TD>
-          <TD><IMG height=2 alt='' src='/_pict/error/undercover.gif' width=1
-            border=0></TD></TR>
-        <TR>
-          <TD vAlign=bottom rowSpan=4 bgcolor='#FFFFFF'><IMG height=43 alt=''
-            src='/_pict/error/ecke.gif' width=14 border=0></TD>
-          <TD vAlign=center align=middle rowSpan=2 bgcolor='#FFFFFF'>
-            <TABLE cellSpacing=1 cellPadding=0 width=470 border=0>
-              <TBODY>
-              <TR>
-                <TD><FONT face='Verdana, Helvetica, sans-serif' color=red
-                  size=4><B>$titre</B></FONT><BR>
-                  <IMG height=5 alt=''
-                  src='/_pict/error/undercover.gif' width=14 border=0><BR></TD></TR>
-              <TR>
-                <TD><FONT face='Verdana, Helvetica, sans-serif' color=black
-                  size=2>$debug_text</FONT></TD></TR></TBODY></TABLE></TD>
-          <TD align=right width=2 rowSpan=2 bgcolor='#FFFFFF'><IMG height=146 alt=''
-            src='/_pict/error/mrblue.gif' width=2 border=0></TD>
-          <TD bgcolor='#FFFFFF'><IMG height=132 alt='' src='/_pict/error/undercover.gif' width=1
-            border=0></TD>
-        </TR>
-        <TR>
-          <TD><IMG height=14 alt='' src='/_pict/error/undercover.gif' width=1
-            border=0></TD></TR>
-        <TR>
-          <TD colSpan=2><IMG height=2 alt='' src='/_pict/error/mrblue.gif'
-            width=486 border=0></TD>
-          <TD><IMG height=2 alt='' src='/_pict/error/undercover.gif' width=1
-            border=0></TD></TR>
-        <TR>
-          <TD colSpan=2><IMG height=27 alt='' src='/_pict/error/undercover.gif'
-            width=486 border=0></TD>
-          <TD><IMG height=27 alt='' src='/_pict/error/undercover.gif' width=1
-            border=0></TD></TR></TBODY></TABLE>
-      <P>&nbsp;</P>
-      </TD></TR></TBODY></TABLE>
-	";
-
-    exit;
+    echo '<div id="fatal_error"><h2 class="fatal_error_title">';
+    echo $title;
+    echo '</h2><p class="fatal_error_message">';
+    echo $debug_text;
+    echo '</p></div';
+    die;
 }
