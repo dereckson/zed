dereckson/zed

commit 10b1a67c2ef752d247013e1151d585add163ea1f

Add declaration and return types in Smarty engine

Engines/Templates/Smarty/Configuration.php
@@ -8,34 +8,19 @@ class Configuration {
     /// Private members
     ///
 
-    /**
-     * @var string
-     */
-    private $applicationDirectory;
+    private string $applicationDirectory;
 
-    /**
-     * @var string
-     */
-    private $cacheDirectory;
+    private string $cacheDirectory;
 
-    /**
-     * @var string
-     */
-    private $theme;
+    private string $theme;
 
-    /**
-     * @var string
-     */
-    private $staticContentURL;
+    private string $staticContentURL;
 
     ///
     /// Singleton
     ///
 
-    /**
-     * @var Engine
-     */
-    private static $instance = null;
+    private static ?Configuration $instance = null;
 
     public static function load () : Configuration {
         if (self::$instance === null) {

Engines/Templates/Smarty/Engine.php
@@ -10,24 +10,15 @@ class Engine {
     /// Private members
     ///
 
-    /**
-     * @var Smarty
-     */
-    private $smarty;
+    private Smarty $smarty;
 
-    /**
-     * @var Configuration
-     */
-    private $config;
+    private Configuration $config;
 
     ///
     /// Singleton
     ///
 
-    /**
-     * @var Engine
-     */
-    private static $instance = null;
+    private static ?Engine $instance = null;
 
     public static function load () : Engine {
         if (self::$instance === null) {
@@ -72,7 +63,7 @@ class Engine {
         return $smarty;
     }
 
-    private static function initializeDefaultVariables (Smarty $smarty) {
+    private static function initializeDefaultVariables (Smarty $smarty): void {
         $smarty->assign("PAGE_CSS", []);
         $smarty->assign("PAGE_JS", []);
     }

commit 5f91de525aa0e46831386eb106f7f662aa382da2

Allow to build Smarty without a theme
Currently, the do.php entry point uses Smarty to load
language files. While T121 could fix that, this change
fixes the regression introduced by 10b1a67c2ef.

This is a follow-up for 10b1a67c2ef.

Engines/Templates/Smarty/Configuration.php
@@ -5,6 +5,12 @@ namespace Zed\Engines\Templates\Smarty;
 class Configuration {
 
     ///
+    /// Constants
+    ///
+
+    const DEFAULT_THEME = "";
+
+    ///
     /// Private members
     ///
 
@@ -37,17 +43,19 @@ class Configuration {
     public static function buildDefaultConfiguration() : Configuration {
         global $Config;
 
-        $engineConfig = (new self)
+        return (new self)
             ->setApplicationDirectory(dirname(__DIR__, 3))
             ->setCacheDirectory(CACHE_DIR)
+            ->setTheme(self::getDefaultTheme())
             ->setStaticContentURL($Config['StaticContentURL']);
+    }
 
+    private static function getDefaultTheme () : string {
         if (defined("THEME")) {
-            // Endpoints like do.php don't have THEME defined.
-            $engineConfig->setTheme(THEME);
+            return THEME;
         }
 
-        return $engineConfig;
+        return self::DEFAULT_THEME;
     }
 
     ///